9644ef48e9ed553bceda7314bc6ddcad9857e76a
trivoroficial <trivoroficial@gmail.com>
2026-07-09 12:28:55 -0300

    Add WhatsAppButton component to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Montserrat } from 'next/font/google'
 import './globals.css'
 import { Header } from '@/components/layout/Header'
 import { Footer } from '@/components/layout/Footer'
+import { WhatsAppButton } from '@/components/layout/WhatsAppButton'
 
 const montserrat = Montserrat({ 
   subsets: ['latin'],
@@ -26,6 +27,7 @@ export default function RootLayout({
         <Header />
         <main className="min-h-screen">{children}</main>
         <Footer />
+        <WhatsAppButton />
       </body>
     </html>
   )
