407ebed5ceb74e5a204725870e5d0fdd01772c1b
trivoroficial <trivoroficial@gmail.com>
2026-07-22 15:35:15 -0300

    Refactor Home component to remove unnecessary div

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-// src/app/page.tsx
 import { Header } from '@/components/layout/Header'
 import { Footer } from '@/components/layout/Footer'
 import { Hero } from '@/components/home/Hero'
@@ -10,7 +9,7 @@ import { CtaFinal } from '@/components/home/CtaFinal'
 
 export default function Home() {
   return (
-    <div className="flex flex-col min-h-screen">
+    <>
       <Header />
       <main className="flex-1">
         <Hero />
@@ -21,6 +20,6 @@ export default function Home() {
         <CtaFinal />
       </main>
       <Footer />
-    </div>
+    </>
   )
 }
