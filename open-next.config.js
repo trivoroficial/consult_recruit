9a504da5cae9d8d756e8bb6131a0b6dd197e0264
trivoroficial <trivoroficial@gmail.com>
2026-07-28 11:15:10 -0300

    Delete open-next.config.js

--- open-next.config.js
+++
@@ -1,9 +0,0 @@
-// open-next.config.js
-/** @type {import('@opennextjs/cloudflare').OpenNextConfig} */
-const config = {
-  platform: 'cloudflare',
-  output: 'standalone',
-  incremental: true,
-}
-
-module.exports = config
