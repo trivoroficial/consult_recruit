1e18502e35fc038dc207c613c899083703f4870e
trivoroficial <trivoroficial@gmail.com>
2026-07-28 09:44:16 -0300

    Update open-next.config.js

--- open-next.config.js
+++ open-next.config.js
@@ -1,7 +1,15 @@
-// open-next.config.ts
+// open-next.config.js
+/** @type {import('@opennextjs/cloudflare').OpenNextConfig} */
 const config = {
   platform: 'cloudflare',
   output: 'standalone',
+  incremental: true,
+  // Configuração para o Next.js 14
+  nextConfig: {
+    images: {
+      domains: ['i.ibb.co'],
+    },
+  },
 }
 
-export default config
+module.exports = config
