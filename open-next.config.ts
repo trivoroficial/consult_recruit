16f4f1bf34d2cf89bb5abe2e75336f327e7d9a32
trivoroficial <trivoroficial@gmail.com>
2026-07-28 09:52:44 -0300

    Update open-next.config.ts

--- open-next.config.ts
+++ open-next.config.ts
@@ -5,6 +5,15 @@ const config: OpenNextConfig = {
   platform: 'cloudflare',
   output: 'standalone',
   incremental: true,
+  // Configuração adicional para evitar erros
+  nextConfig: {
+    images: {
+      domains: ['i.ibb.co'],
+    },
+    reactStrictMode: true,
+    swcMinify: true,
+    transpilePackages: ['lucide-react'],
+  },
 }
 
 export default config
