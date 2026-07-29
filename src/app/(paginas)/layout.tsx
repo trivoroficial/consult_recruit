21289cb3e9df787686f99f5b5eb49aa4d9bdca2c
trivoroficial <trivoroficial@gmail.com>
2026-07-29 13:02:57 -0300

    Refactor layout component to Home with new structure

--- src/app/(paginas)/layout.tsx
+++ src/app/(paginas)/layout.tsx
@@ -1,19 +1,29 @@
-import { ReactNode } from 'react'
+'use client'
+
 import { Header } from '@/components/layout/Header'
 import { Footer } from '@/components/layout/Footer'
+import { Hero } from '@/components/home/Hero'
+import { VagasDestaque } from '@/components/home/VagasDestaque'
+import { EmpresasParceiras } from '@/components/home/EmpresasParceiras'
+import { ComoFunciona } from '@/components/home/ComoFunciona'
+import { Depoimentos } from '@/components/home/Depoimentos'
+import { CtaFinal } from '@/components/home/CtaFinal'
+import { ServicosPremium } from '@/components/home/ServicosPremium'
 
-export default function PaginasLayout({
-  children,
-}: {
-  children: ReactNode
-}) {
+export default function Home() {
   return (
-    <div className="flex flex-col min-h-screen">
+    <>
       <Header />
       <main className="flex-1">
-        {children}
+        <Hero />
+        <VagasDestaque />
+        <ServicosPremium />
+        <EmpresasParceiras />
+        <ComoFunciona />
+        <Depoimentos />
+        <CtaFinal />
       </main>
       <Footer />
-    </div>
+    </>
   )
 }
