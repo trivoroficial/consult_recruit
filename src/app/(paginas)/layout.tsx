463ff5fb55d9c2f847c1ccd97d148239e1c5aafd
trivoroficial <trivoroficial@gmail.com>
2026-07-29 13:53:57 -0300

    Refactor layout component to use children prop

--- src/app/(paginas)/layout.tsx
+++ src/app/(paginas)/layout.tsx
@@ -1,29 +1,13 @@
-'use client'
+import { ReactNode } from 'react'
 
-import { Header } from '@/components/layout/Header'
-import { Footer } from '@/components/layout/Footer'
-import { Hero } from '@/components/home/Hero'
-import { VagasDestaque } from '@/components/home/VagasDestaque'
-import { EmpresasParceiras } from '@/components/home/EmpresasParceiras'
-import { ComoFunciona } from '@/components/home/ComoFunciona'
-import { Depoimentos } from '@/components/home/Depoimentos'
-import { CtaFinal } from '@/components/home/CtaFinal'
-import { ServicosPremium } from '@/components/home/ServicosPremium'
-
-export default function Home() {
+export default function PaginasLayout({
+  children,
+}: {
+  children: ReactNode
+}) {
   return (
-    <>
-      <Header />
-      <main className="flex-1">
-        <Hero />
-        <VagasDestaque />
-        <ServicosPremium />
-        <EmpresasParceiras />
-        <ComoFunciona />
-        <Depoimentos />
-        <CtaFinal />
-      </main>
-      <Footer />
-    </>
+    <div className="min-h-screen bg-[#F8F4E6]">
+      {children}
+    </div>
   )
 }
