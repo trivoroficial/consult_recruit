90ae7aecf1f131a15f1c455dd03d6a18c7ea650b
trivoroficial <trivoroficial@gmail.com>
2026-07-29 11:00:57 -0300

    Update Supabase anon key in server.ts

--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -3,7 +3,7 @@ import { createServerClient } from '@supabase/ssr'
 import { cookies } from 'next/headers'
 
 const supabaseUrl = 'https://dhnmyofmavrsfjtntxjt.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRobm15b2ZtYXZyc2ZqdG50eGp0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM1OTgzMzMsImV4cCI6MjA5OTE3NDMzM30.2Ahc15Mw5FPqm4nMx8xCORAFt3OoobmsVQIjrTjXAhA'
+const supabaseAnonKey = 'sb_publishable_NpAUC2GRqhwUsafIkFo6iQ_4azrbrsC'
 
 export const createClient = () => {
   const cookieStore = cookies()
