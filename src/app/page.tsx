1373def180927c7f8a26f8ac3dd9bbb8c470a303
trivoroficial <trivoroficial@gmail.com>
2026-08-10 10:20:54 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import { Header } from '@/components/layout/Header'
 import { Footer } from '@/components/layout/Footer'
 import { Hero } from '@/components/home/Hero'
 import { VagasDestaque } from '@/components/home/VagasDestaque'
+import { ParaEmpresas } from '@/components/home/ParaEmpresas'
 import { ComoFunciona } from '@/components/home/ComoFunciona'
 import { ServicosPremium } from '@/components/home/ServicosPremium'
 import { CtaFinal } from '@/components/home/CtaFinal'
@@ -15,6 +16,7 @@ export default function Home() {
       <main className="flex-1">
         <Hero />
         <VagasDestaque />
+        <ParaEmpresas />
         <ComoFunciona />
         <ServicosPremium />
         <CtaFinal />
